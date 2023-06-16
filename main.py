ac6cc0f465692110554397f3b233778265ddbadc
arhantalwar <arhantalwar@gmail.com>
2023-06-16 20:46:42 +0530

    first base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -9,14 +11,14 @@ def ocr():
         return jsonify({'error': 'No file uploaded'})
 
     file = request.files['file']
+    image = Image.open(file)
 
     # Call your OCR script to extract text from the image file
-    # extracted_text = your_ocr_script.extract_text(file)
     extracted_text = "YESSSSSSIR"
-
+    text = pytesseract.image_to_string(image)
 
     # Return the extracted text as a JSON response
-    return jsonify({'text': extracted_text})
+    return jsonify({'text': text})
 
 
 @app.route('/ocr2', methods=['GET'])
